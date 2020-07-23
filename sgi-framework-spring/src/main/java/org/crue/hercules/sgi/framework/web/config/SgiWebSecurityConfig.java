90f0cd347f64a5b175d2f1c305f4d099f2785c94
treelogic-hercules <desarrollo.hercules@treelogic.com>
2020-07-23 13:29:56 +0000

    KeyCloak configuration for backend login

--- sgi-framework-spring/src/main/java/org/crue/hercules/sgi/framework/web/config/SgiWebSecurityConfig.java
+++ sgi-framework-spring/src/main/java/org/crue/hercules/sgi/framework/web/config/SgiWebSecurityConfig.java
@@ -7,43 +7,91 @@ import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.crue.hercules.sgi.framework.security.oauth2.client.oicd.userinfo.KeycloakOidcUserService;
 import org.crue.hercules.sgi.framework.security.web.SgiAuthenticationEntryPoint;
 import org.crue.hercules.sgi.framework.security.web.access.SgiAccessDeniedHandler;
+import org.crue.hercules.sgi.framework.security.web.authentication.logout.KeycloakLogoutHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class SgiWebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Autowired
   private ObjectMapper mapper;
 
+  @Autowired
+  private JwtDecoder jwtDecoder;
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.exceptionHandling().accessDeniedHandler(accessDeniedHandler())
-        .authenticationEntryPoint(authenticationEntryPoint()).and()
+    // @formatter:off
+    http
         // CSRF protection by cookie
-        .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+        .csrf()
+        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+      .and()
+        // Translate exceptions to JSON
+        .exceptionHandling()
+        .accessDeniedHandler(accessDeniedHandler())
+      .and()
+        // Configure session management as a basis for a classic, server side rendered application
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+      .and()
         // Require authentication for all requests except for error
-        .authorizeRequests().antMatchers("/error").permitAll().antMatchers("/**").authenticated().and()
+        .authorizeRequests()
+        .antMatchers("/error").permitAll()
+        .antMatchers("/**").authenticated()
+      .and()
+        // Propagate logouts via /logout to Keycloak
+        .logout()
+        .addLogoutHandler(keycloakLogoutHandler())
+      .and()
+        // This is the point where OAuth2 login of Spring 5 gets enabled
+        .oauth2Login()
+          .userInfoEndpoint()
+          .oidcUserService(keycloakOidcUserService())
+        .and()
+      .and()
         // Validate tokens through configured OpenID Provider
-        .oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
+        .oauth2ResourceServer()
+          .jwt()
+          .jwtAuthenticationConverter(jwtAuthenticationConverter())
+        .and()
+      .and();
+    // @formatter:on
+  }
+
+  protected OAuth2UserService<OidcUserRequest, OidcUser> keycloakOidcUserService() {
+    return new KeycloakOidcUserService(jwtDecoder, jwtAuthenticationConverter());
   }
 
   protected JwtAuthenticationConverter jwtAuthenticationConverter() {
     JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
     // Convert realm_access.roles claims to granted authorities, for use in access
     // decisions
-    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new Converter<Jwt, Collection<GrantedAuthority>>() {
+    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter());
+    return jwtAuthenticationConverter;
+  }
+
+  protected Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter() {
+    return new Converter<Jwt, Collection<GrantedAuthority>>() {
       @Override
       @SuppressWarnings("unchecked")
       public Collection<GrantedAuthority> convert(final Jwt jwt) {
@@ -51,8 +99,7 @@ public class SgiWebSecurityConfig extends WebSecurityConfigurerAdapter {
         return ((List<String>) realmAccess.get("roles")).stream().map(SimpleGrantedAuthority::new)
             .collect(Collectors.toList());
       }
-    });
-    return jwtAuthenticationConverter;
+    };
   }
 
   protected AccessDeniedHandler accessDeniedHandler() {
@@ -62,4 +109,9 @@ public class SgiWebSecurityConfig extends WebSecurityConfigurerAdapter {
   protected AuthenticationEntryPoint authenticationEntryPoint() {
     return new SgiAuthenticationEntryPoint(mapper);
   }
+
+  protected KeycloakLogoutHandler keycloakLogoutHandler() {
+    return new KeycloakLogoutHandler(new RestTemplate());
+  }
+
 }
