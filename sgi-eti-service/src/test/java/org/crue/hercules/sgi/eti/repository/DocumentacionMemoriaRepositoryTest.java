dab9555f37e8a6d72f1f22978a5055c9ffdfc648
treelogic-hercules <desarrollo.hercules@treelogic.com>
2020-10-16 12:20:50 +0000

    Corrección
    HERCULES-1254 - Nuevo campo tipo investigación titulada

--- sgi-eti-service/src/test/java/org/crue/hercules/sgi/eti/repository/DocumentacionMemoriaRepositoryTest.java
+++ sgi-eti-service/src/test/java/org/crue/hercules/sgi/eti/repository/DocumentacionMemoriaRepositoryTest.java
@@ -83,8 +83,8 @@ public class DocumentacionMemoriaRepositoryTest {
    */
   private PeticionEvaluacion generarMockPeticionEvaluacion(TipoActividad tipoActividad) {
     return new PeticionEvaluacion(null, "Referencia solicitud convocatoria", "Codigo", "PeticionEvaluacion",
-        tipoActividad, "Fuente financiación", LocalDate.now(), LocalDate.now(), "Resumen", "Valor social", "Objetivos",
-        "DiseñoMetodologico", Boolean.FALSE, Boolean.FALSE, "user-001", Boolean.TRUE);
+        tipoActividad, null, "Fuente financiación", LocalDate.now(), LocalDate.now(), "Resumen", "Valor social",
+        "Objetivos", "DiseñoMetodologico", Boolean.FALSE, Boolean.FALSE, "user-001", Boolean.TRUE);
   }
 
   /**
