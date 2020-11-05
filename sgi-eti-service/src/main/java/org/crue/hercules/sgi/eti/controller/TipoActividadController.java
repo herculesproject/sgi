e3fb6ca856592bfe2bb2a16a56fcc17bcd17a7ab
treelogic-hercules <desarrollo.hercules@treelogic.com>
2020-11-05 16:10:54 +0100

    Añadido permiso en findAll de tipo actividad. HERCULES-1769 tipoactividades 401 Unauthorized

--- sgi-eti-service/src/main/java/org/crue/hercules/sgi/eti/controller/TipoActividadController.java
+++ sgi-eti-service/src/main/java/org/crue/hercules/sgi/eti/controller/TipoActividadController.java
@@ -54,7 +54,7 @@ public class TipoActividadController {
    * @param paging pageable
    */
   @GetMapping()
-  @PreAuthorize("hasAnyAuthorityForAnyUO('ETI-PEV-C-INV', 'ETI-PEV-ER-INV')")
+  @PreAuthorize("hasAnyAuthorityForAnyUO('ETI-PEV-C-INV', 'ETI-PEV-ER-INV', 'ETI-PEV-V')")
   ResponseEntity<Page<TipoActividad>> findAll(@RequestParam(name = "q", required = false) List<QueryCriteria> query,
       @RequestPageable(sort = "s") Pageable paging) {
     log.debug("findAll(List<QueryCriteria> query,Pageable paging) - start");
