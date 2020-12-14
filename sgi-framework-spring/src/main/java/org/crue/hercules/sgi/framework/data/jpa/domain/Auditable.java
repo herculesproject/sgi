bc70d1dee0ce7a861e64f5d89fc82b0e7926029d
treelogic-hercules <desarrollo.hercules@treelogic.com>
2020-12-14 13:04:16 +0000

    Support automatic creation/update record info

---
+++ sgi-framework-spring/src/main/java/org/crue/hercules/sgi/framework/data/jpa/domain/Auditable.java
@@ -0,0 +1,31 @@
+package org.crue.hercules.sgi.framework.data.jpa.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Data;
+
+@MappedSuperclass
+@Data
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+  @CreatedBy
+  protected String createdBy;
+
+  @CreatedDate
+  protected LocalDateTime creationDate;
+
+  @LastModifiedBy
+  protected String lastModifiedBy;
+
+  @LastModifiedDate
+  protected LocalDateTime lastModifiedDate;
+}
