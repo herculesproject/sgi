5886a187273904e68a038de4a3e38493dc22ee68
treelogic-hercules <desarrollo.hercules@treelogic.com>
2021-01-22 10:05:53 +0100

    adecuación test a scripts

--- sgi-eti-service/src/test/java/org/crue/hercules/sgi/eti/integration/TipoTareaIT.java
+++ sgi-eti-service/src/test/java/org/crue/hercules/sgi/eti/integration/TipoTareaIT.java
@@ -24,7 +24,7 @@ import org.springframework.web.util.UriComponentsBuilder;
  * Test de integracion de TipoTarea.
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql
+@Sql(scripts = { "classpath:scripts/tipo_tarea.sql" })
 @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:cleanup.sql")
 @SqlMergeMode(MergeMode.MERGE)
 public class TipoTareaIT extends BaseIT {
