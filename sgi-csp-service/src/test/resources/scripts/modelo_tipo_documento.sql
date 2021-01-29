dd71c9e54e9f53e991b6c1027649ad3bab014642
treelogic-hercules <desarrollo.hercules@treelogic.com>
2021-01-29 08:31:27 +0100

    HERCULES-2314 - [PRO][UG] Prórrogas - fix

--- sgi-csp-service/src/test/resources/scripts/modelo_tipo_documento.sql
+++ sgi-csp-service/src/test/resources/scripts/modelo_tipo_documento.sql
@@ -22,8 +22,8 @@ VALUES (3, 3, 1, null, true);
 
 INSERT INTO csp.modelo_tipo_documento 
 (id, tipo_documento_id, modelo_ejecucion_id, modelo_tipo_fase_id, activo) 
-VALUES (4, 4, 1, 1, true);
+VALUES (4, 11, 1, 1, true);
 
 INSERT INTO csp.modelo_tipo_documento 
 (id, tipo_documento_id, modelo_ejecucion_id, modelo_tipo_fase_id, activo) 
-VALUES (5, 5, 1, null, true);
+VALUES (5, 12, 1, null, true);
