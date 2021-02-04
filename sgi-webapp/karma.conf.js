e49de8b7cfad835f9c543b7c36d53a060fd6caae
treelogic-hercules <desarrollo.hercules@treelogic.com>
2021-02-04 11:31:14 +0100

    Fix: ChromeHeadless have not captured in 60000 ms
    The captureTimeout value represents the maximum boot-up time allowed
    for a browser to start and connect to Karma. If any browser does not get
    captured within the timeout, Karma will kill it and try to launch it again
    and, after three attempts to capture it, Karma will give up.
    
    Default: 60000
    Updated to: 120000

--- sgi-webapp/karma.conf.js
+++ sgi-webapp/karma.conf.js
@@ -9,16 +9,19 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/sgi-webapp'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+    coverageReporter: {
+      dir: require('path').join(__dirname, '<%= relativePathToWorkspaceRoot %>/coverage/<%= appName%>'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ],
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
@@ -26,6 +29,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    captureTimeout: 120000,
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
