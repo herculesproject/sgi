c9023bd2a91adc004d6b20c3b2abc8d413b6e457
treelogic-hercules <desarrollo.hercules@treelogic.com>
2021-05-31 13:46:52 +0200

    Gestion de Errores

--- sgi-eti-service/src/main/java/org/crue/hercules/sgi/eti/config/WebConfig.java
+++ sgi-eti-service/src/main/java/org/crue/hercules/sgi/eti/config/WebConfig.java
@@ -1,5 +1,7 @@
 package org.crue.hercules.sgi.eti.config;
 
+import org.crue.hercules.sgi.framework.problem.spring.boot.autoconfigure.EnableProblem;
+import org.crue.hercules.sgi.framework.spring.context.support.boot.autoconfigure.EnableApplicationContextSupport;
 import org.crue.hercules.sgi.framework.web.config.SgiWebConfig;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +11,8 @@ import org.springframework.context.annotation.Configuration;
  * Framework Web configuration.
  */
 @Configuration
+@EnableProblem
+@EnableApplicationContextSupport
 public class WebConfig extends SgiWebConfig {
 
 }
--- sgi-eti-service/src/test/java/org/crue/hercules/sgi/eti/service/BaseServiceTest.java
+++ sgi-eti-service/src/test/java/org/crue/hercules/sgi/eti/service/BaseServiceTest.java
@@ -1,8 +1,12 @@
 package org.crue.hercules.sgi.eti.service;
 
+import org.crue.hercules.sgi.framework.spring.context.support.boot.autoconfigure.ApplicationContextSupportAutoConfiguration;
 import org.crue.hercules.sgi.framework.test.context.support.SgiTestProfileResolver;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.autoconfigure.context.MessageSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -12,6 +16,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ActiveProfiles(resolver = SgiTestProfileResolver.class)
 @ExtendWith(MockitoExtension.class)
 @ExtendWith(SpringExtension.class)
+@Import({ ValidationAutoConfiguration.class, MessageSourceAutoConfiguration.class,
+    ApplicationContextSupportAutoConfiguration.class })
 public class BaseServiceTest {
 
 }
