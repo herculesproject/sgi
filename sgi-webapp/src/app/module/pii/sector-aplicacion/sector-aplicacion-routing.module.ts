1dd6118938c9d8fe7c37e18de0423a38ae6a2b5f
treelogic-hercules <desarrollo.hercules@treelogic.com>
2021-06-23 10:57:11 +0200

    Creado listado de sectores de aplicacion
    HERCULES-3717 [PII] [Configuración] Listado de sectores de aplicación

--- sgi-webapp/src/app/module/pii/sector-aplicacion/sector-aplicacion-routing.module.ts
+++ sgi-webapp/src/app/module/pii/sector-aplicacion/sector-aplicacion-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 import { SgiRoutes } from '@core/route';
+import { SgiAuthGuard } from '@sgi/framework/auth';
+import { SectorAplicacionListadoComponent } from './sector-aplicacion-listado/sector-aplicacion-listado.component';
+
+const MSG_LISTADO_TITLE = marker('menu.pii.configuraciones.sectores-aplicacion');
 
 const routes: SgiRoutes = [
-
+  {
+    path: '',
+    component: SectorAplicacionListadoComponent,
+    canActivate: [SgiAuthGuard],
+    data: {
+      title: MSG_LISTADO_TITLE,
+      hasAnyAuthority: ['PII-SEA-V', 'PII-SEA-C', 'PII-SEA-E', 'PII-SEA-B', 'PII-SEA-R']
+    }
+  }
 ];
 
 @NgModule({
--- sgi-webapp/src/app/module/pii/sector-aplicacion/sector-aplicacion.module.ts
+++ sgi-webapp/src/app/module/pii/sector-aplicacion/sector-aplicacion.module.ts
@@ -6,9 +6,11 @@ import { TranslateModule } from '@ngx-translate/core';
 import { SgiAuthModule } from '@sgi/framework/auth';
 import { SharedModule } from '@shared/shared.module';
 import { SectorAplicacionRoutingModule } from './sector-aplicacion-routing.module';
+import { SectorAplicacionListadoComponent } from './sector-aplicacion-listado/sector-aplicacion-listado.component';
+import { SectorAplicacionModalComponent } from './sector-aplicacion-modal/sector-aplicacion-modal.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SectorAplicacionListadoComponent, SectorAplicacionModalComponent],
   imports: [
     CommonModule,
     SharedModule,
