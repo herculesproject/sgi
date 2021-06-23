fd33d56c0d519144c7ec42a2286b3099550f51c2
treelogic-hercules <desarrollo.hercules@treelogic.com>
2021-06-23 10:22:23 +0200

    [CON] [PRO] Quitar valor en enumerado Clasificación CVN
    HERCULES-3842 [CON] [PRO] Quitar valor en enumerado Clasificación CVN

--- sgi-webapp/src/app/core/enums/clasificacion-cvn.ts
+++ sgi-webapp/src/app/core/enums/clasificacion-cvn.ts
@@ -4,12 +4,10 @@ export enum ClasificacionCVN {
   AYUDAS = 'AYUDAS',
   COMPETITIVOS = 'COMPETITIVOS',
   NO_COMPETITIVOS = 'NO_COMPETITIVOS',
-  ESTANCIAS = 'ESTANCIAS'
 }
 
 export const CLASIFICACION_CVN_MAP: Map<ClasificacionCVN, string> = new Map([
   [ClasificacionCVN.AYUDAS, marker(`csp.clasificacion-cvn.AYUDAS`)],
   [ClasificacionCVN.COMPETITIVOS, marker(`csp.clasificacion-cvn.COMPETITIVOS`)],
   [ClasificacionCVN.NO_COMPETITIVOS, marker(`csp.clasificacion-cvn.NO_COMPETITIVOS`)],
-  [ClasificacionCVN.ESTANCIAS, marker(`csp.clasificacion-cvn.ESTANCIAS`)]
 ]);
