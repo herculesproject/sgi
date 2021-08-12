d12b8a8c3791e2f3655bd77e1a260f4ba7cfd32f
treelogic-hercules <desarrollo.hercules@treelogic.com>
2021-08-12 14:12:29 +0200

    Fix: Do not fail on test environment
    Create AuthorizedClientServiceOAuth2AuthorizedClientManager only if dependencies have bean created.

--- sgi-framework-spring/src/main/java/org/crue/hercules/sgi/framework/web/config/OAuth2ClientConfiguration.java
+++ sgi-framework-spring/src/main/java/org/crue/hercules/sgi/framework/web/config/OAuth2ClientConfiguration.java
@@ -1,6 +1,7 @@
 package org.crue.hercules.sgi.framework.web.config;
 
 import org.crue.hercules.sgi.framework.http.HttpEntityBuilder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
@@ -22,6 +23,7 @@ public class OAuth2ClientConfiguration {
    * @return a {@link AuthorizedClientServiceOAuth2AuthorizedClientManager}
    */
   @Bean
+  @ConditionalOnBean({ ClientRegistrationRepository.class, OAuth2AuthorizedClientService.class })
   public AuthorizedClientServiceOAuth2AuthorizedClientManager authorizedClientServiceAndManager(
       ClientRegistrationRepository clientRegistrationRepository,
       OAuth2AuthorizedClientService authorizedClientService) {
