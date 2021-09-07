7625ee6a8cb982962460cf589ea938f037b07494
treelogic-hercules <desarrollo.hercules@treelogic.com>
2021-09-07 12:39:18 +0200

    fix: HERCULES-4647 Añadir padreId para CNAE_ROOT
    [SOL][PRO] Añadir clasificación CNAE - No guarda la clasificación CNAE

--- sgi-esb/sgi-esb-sgo/sgo-registry-resources/clasificacion-ids-transformer-mediator.js
+++ sgi-esb/sgi-esb-sgo/sgo-registry-resources/clasificacion-ids-transformer-mediator.js
@@ -5,17 +5,18 @@ function mediate(mc) {
   var log = mc.getServiceLog();
   log.info("clasificacion-ids-transformer-mediator.mediate() - start");
 
+  var isRoot = (mc.getProperty('isRoot') == true);
   var prefix = mc.getProperty('prefix');
   var payload = mc.getPayloadJSON();
   var response = {};
   if (Array.isArray(payload)) {
     var empresasResponse = [];
     payload.forEach(function (empresa) {
-      empresasResponse.push(transformIds(empresa, prefix));
+      empresasResponse.push(transformIds(empresa, prefix, isRoot));
     });
     response = empresasResponse
   } else {
-    response = transformIds(payload, prefix);
+    response = transformIds(payload, prefix, isRoot);
   }
 
   mc.setPayloadJSON(response);
@@ -26,7 +27,7 @@ function mediate(mc) {
 /**
  * Añade el prefijo a los ids de la clasificacion
  */
-function transformIds(clasificacion, prefix) {
+function transformIds(clasificacion, prefix, isRoot) {
   if (prefix) {
     if (clasificacion.id) {
       clasificacion.id = prefix.concat(clasificacion.id);
@@ -34,7 +35,9 @@ function transformIds(clasificacion, prefix) {
     if (clasificacion.padreId) {
       clasificacion.padreId = prefix.concat(clasificacion.padreId);
     }
+    if (prefix == 'CNAE_' && isRoot && clasificacion.padreId == null) {
+      clasificacion.padreId = 'CNAE_ROOT';
+    }
   }
-
   return clasificacion;
 }
