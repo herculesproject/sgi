f8e3fc8fa86f3674e882bfe9e3de3340b6db9aa1
treelogic-hercules <desarrollo.hercules@treelogic.com>
2021-09-29 19:13:36 +0200

    Fix formulario Mxx con múltiples respuestas

--- sgi-webapp/src/app/module/eti/memoria/memoria-formly-form.fragment.ts
+++ sgi-webapp/src/app/module/eti/memoria/memoria-formly-form.fragment.ts
@@ -68,6 +68,7 @@ export abstract class MemoriaFormlyFormFragment extends Fragment {
 
   // Almacena el formState de todos los bloques para poder hacer referencia entre ellos
   private formStateGlobal: any = {};
+  private respuestasSave: IRespuesta[] = [];
 
   isReadonly(): boolean {
     return this.readonly;
@@ -230,11 +231,20 @@ export abstract class MemoriaFormlyFormFragment extends Fragment {
       respuesta = Object.assign(respuesta, field.model);
     });
     question.apartado.respuesta.valor = respuesta;
-    respuestas.push(question.apartado.respuesta);
+
     if (!question.apartado.respuesta.id) {
-      question.apartado.respuesta.memoria = { id: this.getKey() as number } as IMemoria;
-      question.apartado.respuesta.apartado = { id: question.apartado.id } as IApartado;
+      const idMemoria = this.getKey() as number;
+      const idApartado = question.apartado.id;
+      question.apartado.respuesta.memoria = { id: idMemoria } as IMemoria;
+      question.apartado.respuesta.apartado = { id: idApartado } as IApartado;
+
+      const respuestaSaveBefore = this.respuestasSave.find(r => r.memoria?.id === idMemoria && r.apartado?.id === idApartado);
+      if (respuestaSaveBefore) {
+        question.apartado.respuesta.id = respuestaSaveBefore.id;
+      }
     }
+    respuestas.push(question.apartado.respuesta);
+
     const fieldDocumentacion = this.fieldsDocumentacion.get(question.apartado.id);
     if (fieldDocumentacion) {
       const id = fieldDocumentacion.model[fieldDocumentacion.key as string];
@@ -259,6 +269,7 @@ export abstract class MemoriaFormlyFormFragment extends Fragment {
       mergeMap((respuesta) => {
         return this.respuestaService.create(respuesta).pipe(
           map((updated) => {
+            this.respuestasSave.push(updated);
             respuesta = updated;
           })
         );
