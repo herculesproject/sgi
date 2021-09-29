b1e00be984654208ea876c7ccfecc5f6403c245e
treelogic-hercules <desarrollo.hercules@treelogic.com>
2021-09-29 18:54:19 +0200

    Fix pom.xml

--- sgi-rep-service/src/main/java/org/crue/hercules/sgi/rep/dto/OutputReportType.java
+++ sgi-rep-service/src/main/java/org/crue/hercules/sgi/rep/dto/OutputReportType.java
@@ -1,11 +1,12 @@
 package org.crue.hercules.sgi.rep.dto;
 
+import com.google.common.net.MediaType;
+
 /**
  * The supported output types for reports
  */
 public enum OutputReportType {
-  PDF("MediaType.PDF.toString()"), EXCEL("MediaType.MICROSOFT_EXCEL.toString()"),
-  HTML("MediaType.HTML_UTF_8.toString()");
+  PDF(MediaType.PDF.toString()), EXCEL(MediaType.MICROSOFT_EXCEL.toString()), HTML(MediaType.HTML_UTF_8.toString());
 
   public String type;
 
