5acd7b95a2c79b0a789f8da8fec7436aeb7e65c2
treelogic-hercules <desarrollo.hercules@treelogic.com>
2021-10-26 15:36:14 +0200

    Liquibase custom ComputedValueCreateSequenceChange

---
+++ sgi-framework-spring/src/main/java/org/crue/hercules/sgi/framework/liquibase/change/custom/ComputedValueCreateSequenceChange.java
@@ -0,0 +1,132 @@
+package org.crue.hercules.sgi.framework.liquibase.change.custom;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import liquibase.change.core.CreateSequenceChange;
+import liquibase.change.custom.CustomTaskChange;
+import liquibase.database.Database;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.CustomChangeException;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.SetupException;
+import liquibase.exception.UnexpectedLiquibaseException;
+import liquibase.executor.ExecutorService;
+import liquibase.resource.ResourceAccessor;
+import liquibase.statement.SqlStatement;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ComputedValueCreateSequenceChange extends CreateSequenceChange implements CustomTaskChange {
+
+  private String computedStartValue;
+  private String coputedIncrementBy;
+  private String computedMaxValue;
+  private String computedMinValue;
+
+  public String getComputedStartValue() {
+    return computedStartValue;
+  }
+
+  public void setComputedStartValue(String computedStartValue) {
+    this.computedStartValue = computedStartValue;
+  }
+
+  public String getCoputedIncrementBy() {
+    return coputedIncrementBy;
+  }
+
+  public void setCoputedIncrementBy(String coputedIncrementBy) {
+    this.coputedIncrementBy = coputedIncrementBy;
+  }
+
+  public String getComputedMaxValue() {
+    return computedMaxValue;
+  }
+
+  public void setComputedMaxValue(String computedMaxValue) {
+    this.computedMaxValue = computedMaxValue;
+  }
+
+  public String getComputedMinValue() {
+    return computedMinValue;
+  }
+
+  public void setComputedMinValue(String computedMinValue) {
+    this.computedMinValue = computedMinValue;
+  }
+
+  @Override
+  public void execute(Database database) throws CustomChangeException {
+    if (computedStartValue != null) {
+      setStartValue(computeValue(database, computedStartValue));
+    }
+    if (coputedIncrementBy != null) {
+      setIncrementBy(computeValue(database, coputedIncrementBy));
+    }
+    if (computedMaxValue != null) {
+      setMaxValue(computeValue(database, computedMaxValue));
+    }
+    if (computedMinValue != null) {
+      setMinValue(computeValue(database, computedMinValue));
+    }
+
+    SqlStatement[] statements = generateStatements(database);
+    for (SqlStatement statement : statements) {
+      try {
+        ExecutorService.getInstance().getExecutor(database).execute(statement);
+      } catch (DatabaseException e) {
+        throw new UnexpectedLiquibaseException(e);
+      }
+    }
+
+  }
+
+  public BigInteger computeValue(Database database, String sql) throws CustomChangeException {
+    JdbcConnection dbConnection = (JdbcConnection) database.getConnection();
+    PreparedStatement selectStatement = null;
+    PreparedStatement updateStatement = null;
+    ResultSet results;
+
+    try {
+      log.info(sql);
+      selectStatement = dbConnection.prepareStatement(sql);
+      results = selectStatement.executeQuery();
+      if (!results.next()) {
+        throw new UnexpectedLiquibaseException("No results found");
+      }
+      BigDecimal value = results.getBigDecimal(1);
+      if (value == null) {
+        value = BigDecimal.valueOf(0l);
+      }
+      return value.toBigInteger();
+    } catch (Exception e) {
+      throw new CustomChangeException("Cannot create sequence", e);
+    } finally {
+      try {
+        if (selectStatement != null)
+          selectStatement.close();
+        if (updateStatement != null)
+          updateStatement.close();
+      } catch (SQLException e) {
+        log.warn("Database error: ", e);
+      }
+    }
+  }
+
+  @Override
+  public void setUp() throws SetupException {
+    // Do nothing
+
+  }
+
+  @Override
+  public void setFileOpener(ResourceAccessor resourceAccessor) {
+    // Do nothing
+
+  }
+
+}
