a1e3eb8a8c81d5253ad69ba27f0dcd72a7ccc5e8
treelogic-hercules <desarrollo.hercules@treelogic.com>
2021-10-26 11:56:36 +0200

    Fix: Typo

--- sgi-framework-spring/src/main/java/org/crue/hercules/sgi/framework/web/bind/annotation/RequestPageable.java
+++ sgi-framework-spring/src/main/java/org/crue/hercules/sgi/framework/web/bind/annotation/RequestPageable.java
@@ -11,11 +11,11 @@ import java.lang.annotation.Target;
 @Documented
 public @interface RequestPageable {
   public static String DEFAULT_PAGE_HEADER = "X-Page";
-  public static String DEFAIÑT_PAGE_SIZE_HEADER = "X-Page-Size";
+  public static String DEFAULT_PAGE_SIZE_HEADER = "X-Page-Size";
 
   String pageHeader() default DEFAULT_PAGE_HEADER;
 
-  String pageSizeHeader() default DEFAIÑT_PAGE_SIZE_HEADER;
+  String pageSizeHeader() default DEFAULT_PAGE_SIZE_HEADER;
 
   String sort() default "";
 
