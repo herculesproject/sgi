c8a105bd8d5ae182983217ad8f34b0124f135598
treelogic-hercules <desarrollo.hercules@treelogic.com>
2021-11-09 15:51:31 +0000

    HERCULES-3490: [EjecEc] Facturas emitidas
    Se añade pestaña de facturas emitidas.

---
+++ sgi-webapp/src/app/module/csp/ejecucion-economica/ejecucion-economica-formulario/facturas-emitidas/facturas-emitidas.fragment.ts
@@ -0,0 +1,88 @@
+import { FormControl, FormGroup } from '@angular/forms';
+import { IProyecto } from '@core/models/csp/proyecto';
+import { IDatoEconomico } from '@core/models/sge/dato-economico';
+import { IProyectoSge } from '@core/models/sge/proyecto-sge';
+import { ProyectoAnualidadService } from '@core/services/csp/proyecto-anualidad/proyecto-anualidad.service';
+import { ProyectoService } from '@core/services/csp/proyecto.service';
+import { CalendarioFacturacionService } from '@core/services/sge/calendario-facturacion.service';
+import { PersonaService } from '@core/services/sgp/persona.service';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { DesgloseEconomicoFragment, IColumnDefinition, RowTreeDesglose } from '../desglose-economico.fragment';
+
+export class FacturasEmitidasFragment extends DesgloseEconomicoFragment<IDatoEconomico> {
+
+  readonly formGroupFechas = new FormGroup({
+    facturaDesde: new FormControl(),
+    facturaHasta: new FormControl()
+  });
+
+  constructor(
+    key: number,
+    proyectoSge: IProyectoSge,
+    proyectosRelacionados: IProyecto[],
+    proyectoService: ProyectoService,
+    personaService: PersonaService,
+    proyectoAnualidadService: ProyectoAnualidadService,
+    private calendarioFacturacionService: CalendarioFacturacionService
+  ) {
+    super(key, proyectoSge, proyectosRelacionados, proyectoService, personaService, proyectoAnualidadService);
+  }
+
+  protected onInitialize(): void {
+    super.onInitialize();
+
+    this.subscriptions.push(this.getColumns().subscribe(
+      (columns) => {
+        this.columns = columns;
+        this.displayColumns = ['anualidad', 'factura', ...columns.map(column => column.id), 'acciones'];
+      }
+    ));
+  }
+
+  protected getColumns(): Observable<IColumnDefinition[]> {
+    return this.calendarioFacturacionService.getColumnasFacturasEmitidas(this.proyectoSge.id)
+      .pipe(
+        map(response => this.toColumnDefinition(response))
+      );
+  }
+
+  protected getDatosEconomicos(facturaRange?: any): Observable<IDatoEconomico[]> {
+    return this.calendarioFacturacionService.getFacturasEmitidas(this.proyectoSge.id, facturaRange);
+  }
+
+  protected buildRows(datosEconomicos: IDatoEconomico[]): Observable<RowTreeDesglose<IDatoEconomico>[]> {
+    const root: RowTreeDesglose<IDatoEconomico>[] = [];
+    const mapTree = new Map<string, RowTreeDesglose<IDatoEconomico>>();
+    datosEconomicos.forEach(element => {
+      const keyAnualidad = `${element.anualidad}`;
+      const keyFactura = `${keyAnualidad}-${element.id}`;
+      let anualidad = mapTree.get(keyAnualidad);
+      if (!anualidad) {
+        anualidad = new RowTreeDesglose(
+          {
+            anualidad: element.anualidad,
+            id: '',
+            columnas: this.processColumnsValues(element.columnas, this.columns, true)
+          } as IDatoEconomico
+        );
+        mapTree.set(keyAnualidad, anualidad);
+        root.push(anualidad);
+      }
+      let factura = mapTree.get(keyFactura);
+      if (!factura) {
+        factura = new RowTreeDesglose(
+          {
+            anualidad: '',
+            id: element.id,
+            columnas: this.processColumnsValues(element.columnas, this.columns, false)
+          } as IDatoEconomico
+        );
+        mapTree.set(keyFactura, factura);
+        anualidad.addChild(factura);
+      }
+    });
+    return of(root);
+  }
+
+}
