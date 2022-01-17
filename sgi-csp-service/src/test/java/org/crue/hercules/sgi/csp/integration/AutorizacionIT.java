45d67c33b8f7738ab60e6e1b3838d66c7bfe6840
treelogic-hercules <desarrollo.hercules@treelogic.com>
2022-01-17 09:19:42 +0100

    AutorizacionIT SolicitudProyectoResponsableEconomicoIT
    HERCULES-5477

--- sgi-csp-service/src/test/java/org/crue/hercules/sgi/csp/integration/AutorizacionIT.java
+++ sgi-csp-service/src/test/java/org/crue/hercules/sgi/csp/integration/AutorizacionIT.java
@@ -1,6 +1,7 @@
 package org.crue.hercules.sgi.csp.integration;
 
 import java.net.URI;
+import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
 
@@ -44,8 +45,10 @@ class AutorizacionIT extends BaseIT {
   private static final String CONTROLLER_BASE_PATH = AutorizacionController.REQUEST_MAPPING;
   private static final String PATH_PRESENTAR = "/presentar";
   private static final String PATH_PRESENTABLE = "/presentable";
+  private static final String PATH_CAMBIAR_ESTADO = "/cambiar-estado";
+  private static final String PATH_VINCULACIONES_NOTIFICACIONES_PROYECTOS_EXTERNOS = "/vinculacionesnotificacionesproyectosexternos";
 
-  private HttpEntity<Autorizacion> buildRequest(HttpHeaders headers, Autorizacion entity, String... roles)
+  private HttpEntity<Object> buildRequest(HttpHeaders headers, Object entity, String... roles)
       throws Exception {
     headers = (headers != null ? headers : new HttpHeaders());
     headers.setContentType(MediaType.APPLICATION_JSON);
@@ -53,7 +56,7 @@ class AutorizacionIT extends BaseIT {
     headers.set("Authorization", String.format("bearer %s",
         tokenBuilder.buildToken("user", roles)));
 
-    HttpEntity<Autorizacion> request = new HttpEntity<>(entity, headers);
+    HttpEntity<Object> request = new HttpEntity<>(entity, headers);
     return request;
 
   }
@@ -297,6 +300,59 @@ class AutorizacionIT extends BaseIT {
         HttpMethod.DELETE, buildRequest(null, null, "CSP-AUT-B"), Void.class, toDeleteId);
     // then: 204 NO CONTENT
     Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+  }
+
+  @Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = {
+    // @formatter:off
+    "classpath:scripts/modelo_ejecucion.sql",
+    "classpath:scripts/tipo_finalidad.sql",
+    "classpath:scripts/tipo_regimen_concurrencia.sql",
+    "classpath:scripts/tipo_ambito_geografico.sql",
+    "classpath:scripts/convocatoria.sql",
+    "classpath:scripts/autorizacion.sql",
+    "classpath:scripts/estado_autorizacion.sql"
+    // @formatter:on
+  })
+  @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:cleanup.sql")
+  @Test
+  void cambiarEstado_ReturnsAutorizacion() throws Exception {
+    String roles = "CSP-SOL-E";
+    Long idAutorizacion = 1L;
+    EstadoAutorizacion nuevoEstado = EstadoAutorizacion.builder()
+        .id(1L)
+        .estado(EstadoAutorizacion.Estado.REVISION)
+        .autorizacionId(idAutorizacion)
+        .fecha(Instant.now())
+        .comentario("Nuevo estado")
+        .build();
+
+    final ResponseEntity<Autorizacion> response = restTemplate.exchange(
+        CONTROLLER_BASE_PATH + PATH_PARAMETER_ID + PATH_CAMBIAR_ESTADO,
+        HttpMethod.PATCH, buildRequest(null, nuevoEstado, roles), Autorizacion.class, idAutorizacion);
+
+    Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    Autorizacion autorizacion = response.getBody();
+
+    Assertions.assertThat(autorizacion).isNotNull();
+    EstadoAutorizacion estado = this.estadoAutorizacionRepository.findById(autorizacion.getEstadoId()).orElse(null);
+
+    Assertions.assertThat(estado).isNotNull();
+    Assertions.assertThat(estado.getEstado()).isEqualTo(nuevoEstado.getEstado());
+  }
+
+  @Test
+  void hasAutorizacionNotificacionProyectoExterno_ReturnsStatusCode204() throws Exception {
+    String roles = "CSP-AUT-E";
+    Long idAutorizacion = 1L;
+
+    final ResponseEntity<Autorizacion> response = restTemplate.exchange(
+        CONTROLLER_BASE_PATH + PATH_PARAMETER_ID + PATH_VINCULACIONES_NOTIFICACIONES_PROYECTOS_EXTERNOS,
+        HttpMethod.HEAD, buildRequest(null, null, roles), Autorizacion.class, idAutorizacion);
+
+    Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    Autorizacion autorizacion = response.getBody();
+
+    Assertions.assertThat(autorizacion).isNull();
   }
 
   private Autorizacion buildMockAutorizacion(Long id) {
