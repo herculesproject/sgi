046c0b996bb9c923b6755a3e31ad9c5f545f2b92
treelogic-hercules <desarrollo.hercules@treelogic.com>
2022-02-21 14:59:08 +0100

    Securize /inicio-presentacion-justificacion-gastos
    HERCULES-5974 [COM] [PRO] Com - Calendario justificación

--- sgi-csp-service/src/main/java/org/crue/hercules/sgi/csp/controller/ComunicadosController.java
+++ sgi-csp-service/src/main/java/org/crue/hercules/sgi/csp/controller/ComunicadosController.java
@@ -3,6 +3,7 @@ package org.crue.hercules.sgi.csp.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import org.crue.hercules.sgi.csp.service.ComunicadosService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ public class ComunicadosController {
     this.service = service;
   }
 
+  @PreAuthorize("(isClient() and hasAuthority('SCOPE_sgi-csp'))")
   @GetMapping("/inicio-presentacion-justificacion-gastos")
   public void enviarComunicadoInicioPresentacionJustificacionGastos() throws JsonProcessingException {
     log.debug("enviarComunicadoInicioPresentacionJustificacionGastos() - start");
