ad667acc14005ad8fad52782092e1da6ac085374
treelogic-hercules <desarrollo.hercules@treelogic.com>
2022-11-10 10:06:52 +0000

    HERCULES-7730 [CSP][GIN] Equipo - Añadir validaciones dedicación

--- sgi-csp-service/src/main/java/org/crue/hercules/sgi/csp/dto/GrupoEquipoUpdateInput.java
+++ sgi-csp-service/src/main/java/org/crue/hercules/sgi/csp/dto/GrupoEquipoUpdateInput.java
@@ -4,9 +4,14 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.Instant;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.crue.hercules.sgi.csp.model.GrupoEquipo;
 import org.crue.hercules.sgi.csp.model.GrupoEquipo.Dedicacion;
-import org.crue.hercules.sgi.csp.model.RolProyecto.Equipo;
-import org.crue.hercules.sgi.csp.model.RolProyecto.Orden;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,30 +24,30 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class GrupoEquipoOutput implements Serializable {
-
+public class GrupoEquipoUpdateInput implements Serializable {
   private Long id;
-  private String personaRef;
-  private Instant fechaInicio;
-  private Instant fechaFin;
-  private Rol rol;
-  private Dedicacion dedicacion;
-  private BigDecimal participacion;
-  private Long grupoId;
 
-  @Data
-  @EqualsAndHashCode(callSuper = false)
-  @NoArgsConstructor
-  @AllArgsConstructor
-  @Builder
-  public static class Rol implements Serializable {
-    private Long id;
-    private String abreviatura;
-    private String nombre;
-    private Boolean rolPrincipal;
-    private Orden orden;
-    private Equipo equipo;
-    private Boolean activo;
-  }
+  @Size(max = GrupoEquipo.PERSONA_REF_LENGTH)
+  @NotBlank
+  private String personaRef;
+
+  @NotNull
+  private Instant fechaInicio;
+
+  private Instant fechaFin;
+
+  @NotNull
+  private Long rolId;
+
+  @NotNull
+  private Dedicacion dedicacion;
+
+  @Min(GrupoEquipo.PARTICIPACION_MIN)
+  @Max(GrupoEquipo.PARTICIPACION_MAX)
+  @NotNull
+  private BigDecimal participacion;
+
+  @NotNull
+  private Long grupoId;
 
 }
