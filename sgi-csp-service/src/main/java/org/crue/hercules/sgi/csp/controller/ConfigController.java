b00c3181e4a8deae344657464a8e060852cd3849
treelogic-hercules <desarrollo.hercules@treelogic.com>
2024-01-24 13:43:15 +0000

    HERCULES-8815 fix añadir permisos para consultar configuracion desde ejecucion economica y grupos

--- sgi-csp-service/src/main/java/org/crue/hercules/sgi/csp/controller/ConfigController.java
+++ sgi-csp-service/src/main/java/org/crue/hercules/sgi/csp/controller/ConfigController.java
@@ -97,7 +97,7 @@ public class ConfigController {
    * @return ConfigOutput the {@link ConfigParamOutput}
    */
   @GetMapping(PATH_NAME)
-  @PreAuthorize("(isClient() and hasAuthority('SCOPE_sgi-cnf')) or hasAuthority('ADM-CNF-E')")
+  @PreAuthorize("(isClient() and hasAuthority('SCOPE_sgi-cnf')) or hasAnyAuthorityForAnyUO('ADM-CNF-E', 'CSP-EJEC-V', 'CSP-EJEC-E', 'CSP-EJEC-INV-VR', 'CSP-GIN-E', 'CSP-GIN-V', 'CSP-GIN-INV-VR')")
   public ResponseEntity<ConfigParamOutput> get(@PathVariable String name) {
     log.debug("getById(@PathVariable Long id) - start");
     ConfigParamOutput returnValue = service.get(name);
