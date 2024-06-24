d3ed97279f3d1ca83aa045b118d592fb910e0872
treelogic-hercules <desarrollo.hercules@treelogic.com>
2024-06-24 06:24:21 +0000

    HERCULES-9220 - [UPV][ETI] Refactorizar las tablas de tipos (enumerados)

--- sgi-eti-service/src/main/java/org/crue/hercules/sgi/eti/model/TipoTarea.java
+++ sgi-eti-service/src/main/java/org/crue/hercules/sgi/eti/model/TipoTarea.java
@@ -4,7 +4,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,6 +21,33 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TipoTarea extends BaseEntity {
+  public enum Tipo {
+    /** Diseño de proyecto y procedimientos <code>1L</code> */
+    DISENIO_PROYECTO_PROCEDIMIENTOS(1L),
+    /** Eutanasia <code>2L</code> */
+    EUTANASIA(2L),
+    /** Manipulación de animales o realización de procedimientos <code>3L</code> */
+    MANIPULACION_ANIMALES(3L);
+
+    private final Long id;
+
+    private Tipo(Long id) {
+      this.id = id;
+    }
+
+    public Long getId() {
+      return this.id;
+    }
+
+    public static Tipo fromId(Long id) {
+      for (Tipo tipo : Tipo.values()) {
+        if (tipo.id.equals(id)) {
+          return tipo;
+        }
+      }
+      return null;
+    }
+  }
 
   /**
    * Serial version
@@ -39,5 +69,11 @@ public class TipoTarea extends BaseEntity {
   @Column(name = "activo", columnDefinition = "boolean default true", nullable = false)
   @NotNull
   private Boolean activo;
+
+  @JsonIgnore
+  @Transient()
+  public Tipo getTipo() {
+    return Tipo.fromId(this.id);
+  }
 
 }
