d3581dabdbe9628601552b88be418bf05f4d63e6
treelogic-hercules <desarrollo.hercules@treelogic.com>
2024-08-14 14:15:09 +0000

    Fix tests

--- sgi-webapp/src/app/shared/i18n-input/i18n-input.component.ts
+++ sgi-webapp/src/app/shared/i18n-input/i18n-input.component.ts
@@ -1,10 +1,18 @@
+import { FocusMonitor } from '@angular/cdk/a11y';
 import {
+  ChangeDetectorRef,
   Component,
-  forwardRef
+  ElementRef,
+  forwardRef,
+  Optional,
+  Self
 } from '@angular/core';
+import { NgControl } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 import { MatFormFieldControl } from '@angular/material/form-field';
 import { InputI18nBaseComponent } from '@core/component/input-i18n-base/input-i18n-base.component';
+import { LanguageService } from '@core/services/language.service';
 
 let nextUniqueId = 0;
 
@@ -25,5 +33,16 @@ export class I18nInputComponent extends InputI18nBaseComponent {
 
   /** Unique id for this input. */
   protected readonly uid = this.controlType + `-${nextUniqueId++}`;
+
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    elementRef: ElementRef,
+    @Self() @Optional() ngControl: NgControl,
+    defaultErrorStateMatcher: ErrorStateMatcher,
+    languageService: LanguageService,
+    focusMonitor: FocusMonitor
+  ) {
+    super(changeDetectorRef, elementRef, ngControl, defaultErrorStateMatcher, languageService, focusMonitor)
+  }
 
 }
