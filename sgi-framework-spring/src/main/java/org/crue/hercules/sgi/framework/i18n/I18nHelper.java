e4d8e2a7a4160ffa5b9ad502888e437728b5c9d4
treelogic-hercules <desarrollo.hercules@treelogic.com>
2024-09-05 08:29:44 +0200

    Add generic i18n field getter

--- sgi-framework-spring/src/main/java/org/crue/hercules/sgi/framework/i18n/I18nHelper.java
+++ sgi-framework-spring/src/main/java/org/crue/hercules/sgi/framework/i18n/I18nHelper.java
@@ -15,9 +15,8 @@ public class I18nHelper {
 
   /**
    * Get the value of an i18n field that match the current language. If the field
-   * is null
-   * or dosen't contains a value for requested languague <code>null</code> is
-   * returned.
+   * is null or dosen't contains a value for requested languague <code>null</code>
+   * is returned.
    * 
    * @see SgiLocaleContextHolder#getLanguage()
    * @see #getValueForLanguage(Collection, Language)
@@ -39,14 +38,43 @@ public class I18nHelper {
    * @return the value
    */
   public static String getValueForLanguage(Collection<? extends I18nFieldValue> field, Language language) {
-    if (field == null) {
-      return null;
-    }
-    Optional<? extends I18nFieldValue> match = field.stream()
-        .filter(value -> language == value.getLang()).findFirst();
+    Optional<? extends I18nFieldValue> match = getFieldValueForLanguage(field, language);
     if (match.isPresent()) {
       return match.get().getValue();
     }
     return null;
+  }
+
+  /**
+   * Get an i18n field that match the current language. Return value is empty if
+   * the field is null or not contains the current language.
+   * 
+   * @see SgiLocaleContextHolder#getLanguage()
+   * @see #getFieldValueForLanguage(Collection, Language)
+   * 
+   * @param <T>   the type of field, must extends {@link I18nFieldValue}
+   * @param field the i18n field where field would be extracted
+   * @return the field
+   */
+  public static <T extends I18nFieldValue> Optional<T> getFieldValueForCurrentLanguage(Collection<T> field) {
+    return getFieldValueForLanguage(field, SgiLocaleContextHolder.getLanguage());
+  }
+
+  /**
+   * Get an i18n field that match the requested language. Return value is empty if
+   * the field is null or not contains the requested language.
+   * 
+   * @param <T>      the type of field, must extends {@link I18nFieldValue}
+   * @param field    the i18n field where field would be extracted
+   * @param language the language to extract
+   * @return the field
+   */
+  public static <T extends I18nFieldValue> Optional<T> getFieldValueForLanguage(Collection<T> field,
+      Language language) {
+    if (field == null) {
+      return Optional.empty();
+    }
+    return field.stream()
+        .filter(value -> language == value.getLang()).findFirst();
   }
 }
