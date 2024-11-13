b5addbad6a8df789fa8a06e496650d02eacdbd29
treelogic-hercules <desarrollo.hercules@treelogic.com>
2024-11-13 14:07:11 +0000

    [HERCULES-9051] - Fix ConvocatoriaEntidadFinanciadoraConverter, uso de fuenteFinanciacionConverter

--- sgi-webapp/src/app/core/converters/csp/convocatoria-entidad-financiadora.converter.ts
+++ sgi-webapp/src/app/core/converters/csp/convocatoria-entidad-financiadora.converter.ts
@@ -1,6 +1,7 @@
 import { IConvocatoriaEntidadFinanciadoraBackend } from '@core/models/csp/backend/convocatoria-entidad-financiadora-backend';
 import { IConvocatoriaEntidadFinanciadora } from '@core/models/csp/convocatoria-entidad-financiadora';
 import { IEmpresa } from '@core/models/sgemp/empresa';
+import { FUENTE_FINANCIACION_RESPONSE_CONVERTER } from '@core/services/csp/fuente-financiacion/fuente-financiacion-response.converter';
 import { TIPO_FINANCIACION_RESPONSE_CONVERTER } from '@core/services/csp/tipo-financiacion/tipo-financiacion-response.converter';
 import { SgiBaseConverter } from '@sgi/framework/core';
 
@@ -15,7 +16,7 @@ class ConvocatoriaEntidadFinanciadoraConverter extends
       id: value.id,
       empresa: { id: value.entidadRef } as IEmpresa,
       convocatoriaId: value.convocatoriaId,
-      fuenteFinanciacion: value.fuenteFinanciacion,
+      fuenteFinanciacion: FUENTE_FINANCIACION_RESPONSE_CONVERTER.toTarget(value.fuenteFinanciacion),
       tipoFinanciacion: TIPO_FINANCIACION_RESPONSE_CONVERTER.toTarget(value.tipoFinanciacion),
       porcentajeFinanciacion: value.porcentajeFinanciacion,
       importeFinanciacion: value.importeFinanciacion
@@ -30,7 +31,7 @@ class ConvocatoriaEntidadFinanciadoraConverter extends
       id: value.id,
       entidadRef: value.empresa?.id,
       convocatoriaId: value.convocatoriaId,
-      fuenteFinanciacion: value.fuenteFinanciacion,
+      fuenteFinanciacion: FUENTE_FINANCIACION_RESPONSE_CONVERTER.fromTarget(value.fuenteFinanciacion),
       tipoFinanciacion: TIPO_FINANCIACION_RESPONSE_CONVERTER.fromTarget(value.tipoFinanciacion),
       porcentajeFinanciacion: value.porcentajeFinanciacion,
       importeFinanciacion: value.importeFinanciacion
--- sgi-webapp/src/app/core/models/csp/backend/convocatoria-entidad-financiadora-backend.ts
+++ sgi-webapp/src/app/core/models/csp/backend/convocatoria-entidad-financiadora-backend.ts
@@ -1,11 +1,11 @@
+import { IFuenteFinanciacionResponse } from '@core/services/csp/fuente-financiacion/fuente-financiacion-response';
 import { ITipoFinanciacionResponse } from '@core/services/csp/tipo-financiacion/tipo-financiacion-response';
-import { IFuenteFinanciacion } from '../fuente-financiacion';
 
 export interface IConvocatoriaEntidadFinanciadoraBackend {
   id: number;
   entidadRef: string;
   convocatoriaId: number;
-  fuenteFinanciacion: IFuenteFinanciacion;
+  fuenteFinanciacion: IFuenteFinanciacionResponse;
   tipoFinanciacion: ITipoFinanciacionResponse;
   porcentajeFinanciacion: number;
   importeFinanciacion: number;
