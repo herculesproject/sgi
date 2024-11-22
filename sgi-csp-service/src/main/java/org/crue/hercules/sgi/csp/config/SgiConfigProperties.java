11d14903365e864e7051cc13139e8d621da400a6
treelogic-hercules <desarrollo.hercules@treelogic.com>
2024-11-22 12:39:34 +0000

    Add I18n config support

--- sgi-csp-service/src/main/java/org/crue/hercules/sgi/csp/config/SgiConfigProperties.java
+++ sgi-csp-service/src/main/java/org/crue/hercules/sgi/csp/config/SgiConfigProperties.java
@@ -2,7 +2,10 @@ package org.crue.hercules.sgi.csp.config;
 
 import java.util.TimeZone;
 
+import javax.validation.constraints.NotNull;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 import lombok.Data;
 
@@ -11,14 +14,23 @@ import lombok.Data;
  */
 @ConfigurationProperties(prefix = "sgi")
 @Data
+@Validated
 public class SgiConfigProperties {
   /**
    * TimeZone.
    */
+  @NotNull
   private TimeZone timeZone;
-  
+
   /**
    * Web Url.
    */
+  @NotNull
   private String webUrl;
+
+  /**
+   * I18n config refresh interval in seconds. Default 300
+   */
+  @NotNull
+  private Long i18nConfigRefreshInterval = 300L;
 }
