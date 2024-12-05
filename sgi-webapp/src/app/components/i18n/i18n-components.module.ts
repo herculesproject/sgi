ed641f69fe7b225b2ffd359de29d40c2600768ee
treelogic-hercules <desarrollo.hercules@treelogic.com>
2024-12-05 17:29:03 +0000

    i18n tooltip pipe

--- sgi-webapp/src/app/components/i18n/i18n-components.module.ts
+++ sgi-webapp/src/app/components/i18n/i18n-components.module.ts
@@ -7,6 +7,7 @@ import { TranslateModule } from '@ngx-translate/core';
 import { I18nCkeditorComponent } from './i18n-ckeditor/i18n-ckeditor.component';
 import { I18nDisplayFieldLineaInvestigacionComponent } from './i18n-display-field-linea-investigacion/i18n-display-field-linea-investigacion.component';
 import { I18nDisplayFieldComponent } from './i18n-display-field/i18n-display-field.component';
+import { I18nFieldTooltipPipe } from './i18n-field-tooltip.pipe';
 import { I18nFieldValuePipe } from './i18n-field-value.pipe';
 import { I18nInputLineaInvestigacionComponent } from './i18n-input-linea-investigacion/i18n-input-linea-investigacion.component';
 import { I18nInputComponent } from './i18n-input/i18n-input.component';
@@ -19,12 +20,13 @@ import { I18nTreeFieldComponent } from './i18n-tree-field/i18n-tree-field.compon
     I18nCkeditorComponent,
     I18nDisplayFieldComponent,
     I18nDisplayFieldLineaInvestigacionComponent,
+    I18nFieldTooltipPipe,
     I18nFieldValuePipe,
     I18nInputComponent,
+    I18nInputLineaInvestigacionComponent,
     I18nTableFieldComponent,
     I18nTextareaComponent,
-    I18nTreeFieldComponent,
-    I18nInputLineaInvestigacionComponent
+    I18nTreeFieldComponent
   ],
   imports: [
     CommonModule,
@@ -36,14 +38,15 @@ import { I18nTreeFieldComponent } from './i18n-tree-field/i18n-tree-field.compon
   ],
   exports: [
     I18nCkeditorComponent,
-    I18nFieldValuePipe,
     I18nDisplayFieldComponent,
     I18nDisplayFieldLineaInvestigacionComponent,
+    I18nFieldTooltipPipe,
+    I18nFieldValuePipe,
     I18nInputComponent,
+    I18nInputLineaInvestigacionComponent,
     I18nTableFieldComponent,
     I18nTextareaComponent,
-    I18nTreeFieldComponent,
-    I18nInputLineaInvestigacionComponent
+    I18nTreeFieldComponent
   ]
 })
 export class I18nComponentsModule { }
