6e385616e6b6d3a970dfe1da94019e3ece71f21d
treelogic-hercules <desarrollo.hercules@treelogic.com>
2025-09-18 16:11:26 +0000

    [HERCULES-10050] - [UPV] [GEN] Informes se descargan en idioma no seleccionado
    Correctivo - Se corrige defecto en la generación de informes en múltiples idiomas. Los informes se descargaban en un idioma diferente al seleccionado

--- sgi-rep-service/src/main/java/org/crue/hercules/sgi/rep/util/SgiReportContextHolder.java
+++ sgi-rep-service/src/main/java/org/crue/hercules/sgi/rep/util/SgiReportContextHolder.java
@@ -5,30 +5,43 @@ import java.util.TimeZone;
 import org.crue.hercules.sgi.framework.i18n.Language;
 import org.crue.hercules.sgi.framework.spring.context.i18n.SgiLocaleContextHolder;
 import org.crue.hercules.sgi.rep.config.SgiConfigProperties;
-import org.springframework.core.NamedThreadLocal;
 import org.springframework.lang.Nullable;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 
 public class SgiReportContextHolder {
-  private static final ThreadLocal<ReportContext> reportContextHolder = new NamedThreadLocal<>("ReportContext");
+
+  private static final String REPORT_CONTEXT_KEY = "SgiReportContext";
 
   private SgiReportContextHolder() {
   }
 
   private static void resetReportContext() {
-    reportContextHolder.remove();
+    RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+    if (requestAttributes != null) {
+      requestAttributes.removeAttribute(REPORT_CONTEXT_KEY, RequestAttributes.SCOPE_REQUEST);
+    }
   }
 
   private static void setReportContext(@Nullable ReportContext reportContext) {
     if (reportContext == null) {
       resetReportContext();
     } else {
-      reportContextHolder.set(reportContext);
+      RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+      if (requestAttributes != null) {
+        requestAttributes.setAttribute(REPORT_CONTEXT_KEY, reportContext, RequestAttributes.SCOPE_REQUEST);
+      }
     }
   }
 
   @Nullable
   private static ReportContext getReportContext() {
-    return reportContextHolder.get();
+    RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+    if (requestAttributes != null) {
+      return (ReportContext) requestAttributes.getAttribute(REPORT_CONTEXT_KEY, RequestAttributes.SCOPE_REQUEST);
+    }
+
+    return null;
   }
 
   public static void setLanguage(@Nullable Language language) {
